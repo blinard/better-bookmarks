192fe073feaae963889872c01568053be762c22d
bradlinard <brad.linard@idelata.com>
2017-11-11 21:32:27 -0800

    adding clean

--- gulpfile.js
+++ gulpfile.js
@@ -66,3 +66,78 @@ gulp.task('build:chromeextension', ['build:models', 'build:dataaccess', 'build:b
 
 gulp.task('build', ['build:chromeextension']);
 
+// Cleans
+gulp.task('clean:models', function(cb) {
+    var child = spawn('gulp', ['clean:dist'], { stdio: 'inherit', cwd: './bb.models' });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            cb('an error occurred');
+            return;
+        }
+
+        cb();
+    });    
+});
+
+gulp.task('clean:dataaccess', [], function(cb) {
+    var child = spawn('gulp', ['clean:dist'], { stdio: 'inherit', cwd: './bb.dataaccess' });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            cb('an error occurred');
+            return;
+        }
+
+        cb();
+    });    
+});
+
+gulp.task('clean:business', [], function(cb) {
+    var child = spawn('gulp', ['clean:dist'], { stdio: 'inherit', cwd: './bb.business' });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            cb('an error occurred');
+            return;
+        }
+
+        cb();
+    });    
+});
+
+gulp.task('clean:bfchrome', [], function(cb) {
+    var child = spawn('gulp', ['clean:dist'], { stdio: 'inherit', cwd: './bb.browserfacades.chrome' });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            cb('an error occurred');
+            return;
+        }
+
+        cb();
+    });    
+});
+
+gulp.task('clean:uioptions', [], function(cb) {
+    var child = spawn('gulp', ['clean:dist'], { stdio: 'inherit', cwd: './bb.ui.options' });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            cb('an error occurred');
+            return;
+        }
+
+        cb();
+    });    
+});
+
+gulp.task('clean:chromeextension', [], function(cb) {
+    var child = spawn('gulp', ['clean:dist'], { stdio: 'inherit', cwd: './chrome-extension' });
+    child.on('exit', function(code) {
+        if (code !== 0) {
+            cb('an error occurred');
+            return;
+        }
+
+        cb();
+    });    
+});
+
+gulp.task('clean', ['clean:chromeextension', 'clean:uioptions', 'clean:bfchrome', 'clean:business', 'clean:dataaccess', 'clean:models']);
+//end cleans
