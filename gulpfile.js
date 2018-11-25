b76cd90150d2842b8d6e0e070872d5436d20e55e
bradlinard <brad.linard@idelata.com>
2018-11-25 12:40:13 -0800

    Adding build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,36 @@
 var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
-var concat = require("gulp-concat");
+var exec = require('child_process').exec;
 
-gulp.task("bundle:listeners", function () {
-  return gulp.src("src/omniboxListeners.js")
+function cmd(cmdString, cb) {
+    exec(cmdString, function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+}
+
+gulp.task("build:js", function () {
+  return gulp.src("src/**/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("all.js"))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy:html", function() {
+    return gulp.src("src/**/*.html")
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy:manifest", function() {
+    return gulp.src("manifest.json")
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task("build", ["build:js", "copy:html", "copy:manifest"]);
+
+gulp.task("clean", function(cb) {
+    cmd('rm -rf dist', cb);
 });
