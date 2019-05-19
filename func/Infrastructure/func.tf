040d3922d56f1125e066d4dfa595dc534fe08af2
Brad Linard <brad.linard@idelata.com>
2019-05-19 02:17:14 -0700

    Adding TerraForm config for IaC

---
+++ func/Infrastructure/func.tf
@@ -0,0 +1,57 @@
+provider "azurerm" {
+  subscription_id = "${var.azure_subscription_id}"
+  version = "~> 1.28"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "${var.azure_resource_group_name}"
+  location = "${var.azure_region}"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name = "${var.azure_storage_name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location = "${azurerm_resource_group.rg.location}"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name = "${var.azure_app_insights_name}"
+  location = "westus"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  application_type = "Web"
+}
+
+
+resource "azurerm_app_service_plan" "asp" {
+  name = "${var.azure_app_service_plan_name}"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  kind = "FunctionApp"
+
+  sku {
+      tier = "Dynamic"
+      size = "Y1"
+  }
+}
+
+# Something needs to be set in the Azure Function so that Application Insights will collect the logs
+# Think the AppInsights id or something needs set in the Function App
+# Actually - it might be the App Service that it needs configured with
+resource "azurerm_function_app" "func" {
+  name = "${var.azure_function_name}"
+  location = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+  storage_connection_string = "${azurerm_storage_account.storage.primary_connection_string}"
+  "version" = "~2"
+  
+  app_settings {
+    "FUNCTIONS_WORKER_RUNTIME" = "dotnet"
+    "FUNCTION_APP_EDIT_MODE" = "readonly"
+    "APPINSIGNTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.appinsights.instrumentation_key}"
+    "https_only" = "true"
+  }
+}
+
---
+++ func/Infrastructure/variables.tf
@@ -0,0 +1,28 @@
+variable "azure_subscription_id" {
+  default = "b71ca46d-c660-4a33-8405-9a9507937687"
+}
+
+variable "azure_region" {
+  default = "centralus"
+}
+
+variable "azure_resource_group_name" {
+  default = "bb-tf-rg"
+}
+
+variable "azure_storage_name" {
+    default = "bbfunctionstorage"
+}
+
+variable "azure_function_name" {
+    default = "bbfunction"
+}
+
+variable "azure_app_service_plan_name" {
+  default = "bbserviceplan"
+}
+
+variable "azure_app_insights_name" {
+  default = "bbserviceinsights"
+}
+
